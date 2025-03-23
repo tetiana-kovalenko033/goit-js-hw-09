a6e2a5e2a4834a212a3e2110a4aea62da80ca907
Tetiana <kovalenko_t@ukr.net>
2025-03-23 16:05:32 +0200

    add js code

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,40 @@
-formData = { email: "", mesage: "" }
+
+const STORAGE_KEY = "feedback-form-state";
 
 const form = document.querySelector(".feedback-form");
-const input = form.querySelector("input");
-const textArea = form.querySelector("textArea");
-const button = form.querySelector("button");
 
-// form.addEventListener("input", => {
-//     email = event.target.value.email,
-//     mesage = event.target.value.mesage
-// })
+let formData = { email: "", message: "" };
+
+
+
+function handleInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+     console.log(event);
+      
+}
+form.addEventListener("input", handleInput);
+
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        formData = JSON.parse(savedData);
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    }
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    if (!formData.email || !formData.message) {
+        alert("Fill please all fields");
+        return ;        
+    }
+       
+    console.log(formData);
+    
+localStorage.removeItem(STORAGE_KEY);
+formData = { email: "", message: "" };
+form.reset();
+}
+
+form.addEventListener("submit", handleSubmit)
