4179799b6bfca93839422fb87d0437ce9ff7b8e1
tetiana-kovalenko033 <kovalenko_t@ukr.net>
2025-03-23 11:14:28 +0000

    Deploying to gh-pages from @ tetiana-kovalenko033/goit-js-hw-09@f8a4b0d4abcb652df1eb860427b895932135490f 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DyNsYEw-.js";formData={email:"",mesage:""};const e=document.querySelector(".feedback-form");e.querySelector("input");e.querySelector("textArea");e.querySelector("button");
+//# sourceMappingURL=2-form.js.map
